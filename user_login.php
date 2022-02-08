d9c12b8852d88bf808ae9cdd0e213829ac8d32f2
Proffesuer <levischisira0@gmail.com>
2022-02-08 14:14:58 +0300

    Adding also my loginStyle sheet page

--- user_login.php
+++ user_login.php
@@ -3,9 +3,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>User log in page</title>
+    <link type="text/css" href="loginStyle.css"rel="stylesheet">
+    
 </head>
 <body>
+
+
+
+
+
+
+
+
+
     
 </body>
 </html>
