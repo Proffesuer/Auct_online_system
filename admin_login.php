e0a52b9825bf248d518a4725395f7260c510efb9
Proffesuer <levischisira0@gmail.com>
2022-02-08 17:03:36 +0300

    Setting up the rough idea and structure of the system

--- admin_login.php
+++ admin_login.php
@@ -1,14 +1,27 @@
+<?php
+
+
+
+
+
+
+?>
+
+
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin log in page</title>
-    <link type="text/css" href="loginStyle.css"rel="stylesheet">
+    <link rel="stylesheet"type="text/css"href="css/loginStyle.css" />
 </head>
 <body>
 
-
+<h1>
+    hello guys am head
+</h1>
 
     
 </body>
--- user_login.php
+++ user_login.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User log in page</title>
-    <link type="text/css" href="loginStyle.css"rel="stylesheet">
+    <link type="text/css" href="css/loginStyle.css"rel="stylesheet">
     
 </head>
 <body>
