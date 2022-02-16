61635bbdd9ffb0cc0c620952dca4b676e9c88812
Proffesuer <levischisira0@gmail.com>
2022-02-16 14:35:32 +0300

    making signup form for the user

--- signupUser.php
+++ signupUser.php
@@ -4,10 +4,36 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User sign up</title>
-    <link rel="stylesheet" href="css/signup.css"type="text/css">
+    <link rel="stylesheet" href="user.css"type="text/css">
     
 </head>
 <body>
+<strong><h1 align="center"class="colorr">TECHNOMOBIZ</h1></strong>
+
+<di class="bg3">
+    <form >
+        <lable>
+            Email:
+</lable>
+
+<input type="text" name="email"id="mail"placeholder="Enter email"required><br>
+<lable>
+    Password:
+</lable>
+<input type="password"name="password"required placeholder="Password"><br>
+<input type="submit" name="submit" class="button" value="login" />
+
+
+
+</form>
+
+
+
+
+
+</div>
+
+
     
 </body>
 </html>
