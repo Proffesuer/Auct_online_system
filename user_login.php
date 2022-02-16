a5a944d854a701171dc924e6fc5bb30bfc68513f
Proffesuer <levischisira0@gmail.com>
2022-02-16 17:24:20 +0300

    Created the user login form--added a footer below

--- user_login.php
+++ user_login.php
@@ -4,13 +4,54 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User log in page</title>
-    <link type="text/css" href="css/loginStyle.css"rel="stylesheet">
+    <link type="text/css" href="user.css"rel="stylesheet">
     
 </head>
 <body>
 
 
 
+<strong><h1 align="center"class="color1">TECHNOMOBIZ</h1></strong>
+<center>
+
+    <form class="bg3">
+        <lable>
+            USER LOG IN
+</lable>
+<BR>
+<BR>
+        
+        
+            Email:
+</lable><br>
+
+<input type="text" name="email"id="mail"placeholder="Enter email"required><br>
+<lable>
+    Password:
+</lable><br>
+<input type="password"name="password"required placeholder="Password"><br>
+
+<br>
+<br>
+<input type="submit" name="submit" class="button" value="login" />
+
+
+
+</form>
+<br>
+
+
+</center>
+
+
+<div class="footer">
+
+hello guys this is my footer come lets checkout
+
+
+
+
+</div>
 
 
 
