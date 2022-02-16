6721a56822228a3908d8b7db1e49c459dbf9fc00
Proffesuer <levischisira0@gmail.com>
2022-02-16 13:19:58 +0300

    staging the new untracked file of the project

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 
-
-<?php
-
-
-
-?>
-
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
+    <link rel="stylesheet"href="Auct_style.css"type="text/css">
 </head>
 <body>
+    
 <h>Hello  bro congrats now what we have done is what will be doing every day 
 you edit your page you add and commit the push(when online bro) kama hauna 
 net utakuwa una add commit alafu unapush tu ukiwa online  so use your vscode 
@@ -26,6 +21,8 @@ kama editing tool then  git bash kupush</h>
 
 <h3> checking </h3>
 
-    
+</div> 
+
+
 </body>
 </html>
