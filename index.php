be8d15d661ca7466848b7045ca41b25c12d7a429
Ben <bensonmakokha25@gmail.com>
2022-02-23 14:32:14 +0300

    added scriptss

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+
+session_start();
+
+if(isset($_SESSION['FirstName'])){
+
+    $_SESSION['msg'] = "you must log in to view this page";
+    header("location: admin_login");
+
+}
+
+if(isset($_GET['logout'])){
+
+
+    session_destroy();
+    unset($_SESSION['FirstName']);
+    header("location: admin_login");
+
+
+}
+
+
+
+?>
+
+
+
+
 
 <html lang="en">
 <head>
@@ -8,6 +37,53 @@
     <link rel="stylesheet"href="Auct_style.css"type="text/css">
 </head>
 <body>
+
+<h1> THIS IS HOME PAGE</h1>
+
+<?php
+
+
+if(isset($_SESSION['success'])) : ?>
+
+<div>
+
+<h2>
+
+
+<?php
+
+echo$_SESSION['success'];
+unset($_SESSION['success']);
+
+
+?>
+
+</h2>
+
+
+
+</div>
+<?php endif ?>
+
+
+<?php if(isset($_SESSION['FirstName'])); : ?>
+
+     <h2> <strong> welcome <?php echo $_SESSION['FirstName'] ?> </strong></h2> 
+
+     <button> <a href = "index.php?logout= '1'"</a></button>
+     <?php endif ?>  
+<p> animate </p>
+
+
+<div>
+
+
+
+
+
+
+
+</div>
     
 <h>Hello  bro congrats now what we have done is what will be doing every day 
 you edit your page you add and commit the push(when online bro) kama hauna 
