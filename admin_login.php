b2e57f5b308264ef8210b1e7a907ac2510c44f72
Proffesuer <levischisira0@gmail.com>
2022-03-01 17:42:29 +0300

    changes the entire login form style --background

--- admin_login.php
+++ admin_login.php
@@ -1,201 +1,43 @@
-<?php
-
-if(isset($_POST['button']))
-{
-$UserMail = mysqli_real_escape_string($db, $_POST['UserMail']);
-
-$Password = mysqli_real_escape_string($db, $_POST['Password']);
-
-
-
-if(empty($UserMail))
-
-{
-  array_push($errors, "User email required");
-}
-
-
-
-if(empty($Password))
-
-{
-  array_push($errors, "Password is required");
-}
-
-
-if(count($errors) == 0)
-
-{
-
-$Password = md5($Password); # encrypts password
-
-$query = "SELECT *FROM administrators WHERE Email = '$UserMail' AND Password = '$Password' ";
-
-$results = $mysqli_query($db, $query);
-if(mysqli_num_rows($results)){
-
-
-  $_SESSION[ 'UserMail'] = $UserMail;
-  $_SESSION['success'] = "LOGED IN";
-
-      #login page
-
-      header('location: index.php');
-
-
-}
-}
-else{
-
-  array_push($errors, "Invalid UserMail and Password! Try again");
-
-
-}
-}
-
-
-?>
-
-
-
-
-
-?>
+<?php include('server.php') ?>
 
 
 
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel = "stylesheet" type = "text/css" href = "Admin.css">
-    <title>
-      Admin Log_in page
-</title>
-
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin_login</title>
+  <link rel= "stylesheet" type= "text/css"  href= "admin_login.css" />
 </head>
-<body id = "backimg">
-
-  <strong><h1 align="center"class="colorr">TECHNOMOBIZ</h1></strong>
-
-
-<button onclick="document.getElementById('id01').style.display='block'" style="width:250px; align-items:center;">Login</button>
-
-<div id="id01" class="modal">
-
-<form class="modal-content animate" action="server.php" method="post">
-
-
-  <div class="container">
-    
-    <label for="UserMail"><b>UserMail</b></label>
-  
-    <input type="text" placeholder="Enter Usermail" name="UserMail" required>
-
-    <br>
-
-    <label for="Password"><b>Password</b></label>
-    <input type="Password" placeholder="Enter Password" name="Password" required>
-
-    <br>
-      
-
-      <input type="checkbox" checked="checked" name="remember"> Remember me 
-
-    </label>
-
-    <br>
-      <br>
-
-    <button type="button">Login</button>
-
-    <br>
-    <label>
-</div>
+<body>
+<center>
+  <div class="container"align="center">
 
   
-    <div id =" Loginfailed" >
-
-
-
-          <p> invalid usermail and password! create account? </p>
-
-
-
-
-</div>
+  <lable>
+LOG IN
+</lable>
+<form method="POST"action= "admin_login.php">
 
 <br>
 
-  <div class="container">
-    <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-    <a href="CreateAccount.php"><button type="button"  class="cancelbtn">Create Account</button></a>
+<lable>
+Email:
+</lable >
+<br><input type="text"name="Email"placeholder="Enter Email"required>
 
-    <br>
-    <br>
-    <br>
-    <span col= "4" class="psw">Forgot <a href="#">password?</a></span>
-  </div>
+<br><br>
+<lable>
+Password:
+</lable>
+<br>
+<input type="password"name="password"placeholder="Password"required>
+<br><br>
+<input type="submit" name="button"class="Button1"value="LOGIN" />
+
 </form>
 </div>
-
-<div class = "myfooter">
-
-
-<p style = " Margin-left:15%;"> 
-<br>
-
-Welcome administrator!    feel free to login,     create account or update anything in the system as 
-an    Administrator..
-
-
-         <br>
-
-</p>
-
-
-<hr style = "color : red;"> <br>
-<p style = "font-size: 75%;"> .......c..f..y..  
-@DEVELOPER BEN_ALWANGE__&@DEVELOPER PROFESSEUR LEVIS CHISIRA
-<br>
-tel: +254768743827 mail- @gmail.com
-</P>
-
-
-
-
-</div>
-
-
-<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-      modal.style.display = "none";
-  }
-}
-</script>
-
-<script>
-// JavaScript Document
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-  document.getElementById("navbar").style.top = "0";
-} else {
-  document.getElementById("navbar").style.top = "-50px";
-}
-prevScrollpos = currentScrollPos;
-}
-
-
-<script src="curtains.js"></script>
+</center>
 
 </body>
 </html>
