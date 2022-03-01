1860b26adf5f79e9e54a56a5ab0962542665e505
Proffesuer <levischisira0@gmail.com>
2022-03-01 12:45:29 +0300

    removed phpcodes---required-in the form

--- CreateAccount.php
+++ CreateAccount.php
@@ -2,6 +2,7 @@
 <?php include('server.php') ?>
 
 
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,8 +19,9 @@
 <strong><h1 align="center"class="colorr">TECHNOMOBIZ</h1></strong>
 <center>
 
-  <form method = "POST" action= "CreateAccount.php" id="back">
+  <form method = "POST" action= "admin_login.php" id="back">
   <?php include('AdminErrors_File.php') ?>
+  
 	<h4 align="center"style="color:white">CREATE ADMIN ACCOUNT</h4>
 
 	<div class="back">
@@ -29,26 +31,25 @@
 			<br>
 			
 
-            <input type="text" class="input_text" name="SecondName" placeholder="LastName"value="<?php ?>" required />
+            <input type="text" class="input_text" name="SecondName" placeholder="LastName"value="<?php ?>" />
 			<br>
            
-            <input type="text" class="input_text" name="UserName" placeholder="UserName"value="<?php ?>" required/>
+            <input type="text" class="input_text" name="UserName" placeholder="UserName"value="<?php ?>"/>
 			</label>
 			<br>
             
-            <input type="text" class="input_text" name="Email" placeholder="Email"value="<?php ?>" required />
+            <input type="text" class="input_text" name="Email" placeholder="Email"value="<?php ?>"/>
             </label>
 			<br>
 			
-            <input type="password" class="input_text" placeholder="Password"name="Password" value="<?php ?>"
+            <input type="password" class="input_text" placeholder="Password"name="Password" 
 			 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-			 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-			 required />
+			 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"/>
 			</label>
 			<br>
 			
-            <input type="password" class="input_text" name="ConfirmPassword" placeholder="Confirm password"value="<?php ?>" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number 
-			and one uppercase and lowercase letter, and at least 8 or more characters" required />
+            <input type="password" class="input_text" name="ConfirmPassword" placeholder="Confirm password"pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number 
+			and one uppercase and lowercase letter, and at least 8 or more characters" />
 			<br>           
 			<input type="submit" name="submit" class="button" value="Register" /><br>
 		   <div class="login">
