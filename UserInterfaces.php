815a35c7e80a7315f6b1dfaef558ff0b691063c0
Ben <bensonmakokha25@gmail.com>
2022-05-04 15:36:29 +0300

    added scriptss

---
+++ UserInterfaces.php
@@ -0,0 +1,145 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en" id= "loadEveryTime">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel = "stylesheet" type= "text/css" href = "user_inter.css">
+
+
+
+</head>
+
+
+
+<body>
+    
+<strong>
+    <h1> ONLINE AUCTION SYSTEM </h>
+
+</strong>
+
+<h3> WE ENABLE YOU ADVERTISE YOUR PRODUCTS AND TRANSACT ONLINE </h3>
+
+<br>
+<h3>
+<marquee  behavior="alternate"> <a><font color="RED">WELCOME ALL! LETS TALK BUSINESS</font></a></marquee>
+</h3>
+
+                <div class= "usebody">
+                    <div class = glow>
+                   <p>    
+                        WE BUY AND SELL HERE TO ANYONE, CREATE ACCOUNT FOR NEW MEMBERS OR LOGIN AND EXPLORE THE WORLD OF BUSINESS HERE
+
+</p>
+</div>
+</div>
+
+
+<div class= "item1">
+
+<img src = "/pic/phones" alt = "mobile phone" width = "0 " height = "0 " >
+
+</div>
+
+
+<div class = "item2">
+
+
+
+<img src = "phones.jpg" alt = "mobile phone" width = " " height = " " >
+
+
+
+</div>
+
+
+<div class = "item3">
+
+
+
+<img src = "phones.jpg" alt = "mobile phone" width = " " height = " " >
+
+
+
+</div>
+
+
+<div class = "item4">
+    
+
+<img src = "phones.jpg" alt = "mobile phone" width = " " height = " " >
+
+
+
+</div>
+
+
+
+<div class = "mylinks">
+        <a href = "index.php" target = "_blank" title = "Online Auction System">login? </a>
+            <br>
+        <a href = "index.php" target= "_blank" title = "Online Auction System">click here to create account </a>
+        <br>
+</div>
+
+
+<div class = "myfooter">
+
+<p style = "padding-left:130px;">
+
+  <br> 
+Developper Ben-Alwanga&Levis Chisira, c- Tel +25468743827   Nairobi. <span> Email-- bensonmakokha25@gmail.com& levischisira@gmail.com</span>
+
+</p>
+
+</div>
+
+
+
+<script type= "text/javascript">
+    exports.userInterface = function firstPageAlways()
+    {
+    const container;
+    document.getElementById('loadEveryTime') = container;
+window.addeventlistener('load,container')
+    }
+
+    </script>
+</body>
+</html>
