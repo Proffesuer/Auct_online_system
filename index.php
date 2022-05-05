01c0ac1760acc5900e7670480a3da5ef1785604d
Proffesuer <levischisira0@gmail.com>
2022-05-05 22:28:58 +0300

    Created a navigation bar

--- index.php
+++ index.php
@@ -1,24 +1,43 @@
 
 
-
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
-    <title>Home</title>
-    <link rel="stylesheet"href="Auct_style.css"type="text/css">
 
+<link rel= "stylesheet" type= "text/css"  href= "home.css" />
 </head>
-
-<body>
-
-
-
+<!--Body-->
+<body >
+<!--animation at the topp of the home page-->
+<div class="back">
 <div class="usebody">
-<h2 class="glow">WELCOME TO E-SOKONI<br> JOIN WITH US TODAY <br> WELCOME ALL</h2>
+<h2 class="glow">WELCOME TO E-SOKONI<br> WELCOME ALL!!</h2>
 </div>
+
+<!--navigation bar with Drop Down menu-->
+
+<div class="navbar">
+  <a href="#home">HOME</a>
+  <a href="#news">CONTACT</a>
+  <div class="dropdown">
+    <button class="dropbtn">LOGIN </button>
+  </div> 
+</div>
+</div>
+<h3>Dropdown Menu inside a Navigation Bar</h3>
+<p>Hover over the "Dropdown" link to see the dropdown menu.</p>
+<footer class="footer">
+hello guys this is my footer
+
+
+
+
+</footer>
+
+
+
 
 </body>
 
+<!-- Mirrored from www.w3schools.com/howto/tryit.asp?filename=tryhow_css_dropdown_navbar by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 27 Jan 2020 02:32:21 GMT -->
 </html>
